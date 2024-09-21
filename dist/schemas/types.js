88960256f12cf2e76856c19b630b33712ac3bfd5
SwatejReddy <swatejreddy17@gmail.com>
2024-09-21 22:55:20 +0530

    Remove dist folder.

--- dist/schemas/types.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// export interface IApiResponse {
-//     statusCode: number;
-//     data: any,
-//     message: string;
-//     success: boolean;
-// }
